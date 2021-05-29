60d331bfb4be128a279ddb95de2b280ae1f62caa
Christopher Susie <cpsusie@hotmail.com>
2021-05-29 11:25:57 -0400

    Now building in vs

---
+++ cjmolly_lib/cjmolly_lib.cpp
@@ -0,0 +1,10 @@
+// cjmolly_lib.cpp : Defines the functions for the static library.
+//
+
+#include <string>
+
+// TODO: This is an example of a library function
+std::string get_str()
+{
+	return std::string{ "Foobar!" };
+}
