2cddfce064d4c44365271cb4593ae5b6491dd0e0
Christopher Susie <cpsusie@hotmail.com>
2021-05-30 16:14:51 -0400

    Now I'm starting to think I won't be able to separate synchronized from the rest of folly .... sigh

--- test_app/program.cpp
+++ test_app/program.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
-
+#include <folly/Synchronized.h>
 int main()
 {
 	constexpr auto newl = '\n';
 	std::cout << "Hello, world!" << newl;
+
+	auto vault = folly::Synchronized<long>{ 7 };
+	{
+		auto lck = vault.contextualLock();
+		*lck *= 19;
+	}
+	{
+		auto lck = vault.contextualLock();
+		std::cout << "The value is: [" << *lck << "]." << newl;
+	}
+	
 	return 0;
 }
